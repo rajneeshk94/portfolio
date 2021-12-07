b7c9a6123ac59b326fe9131b635cb478a46a83f2
Rajneesh Khare <rajneeshkhare94@gmail.com>
2021-12-07 16:44:55 +0530

    added form reset

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -80,7 +80,8 @@
     thisForm.querySelector('.loading').classList.remove('d-block');
     thisForm.querySelector('.error-message').innerHTML = "Thank you, will get back to you soon &#128515;";
     thisForm.querySelector('.error-message').classList.add('d-block');
-    document.querySelectorAll('.php-email-form').reset();
+    var frm = document.querySelectorAll('.php-email-form');
+    frm.reset();
   }
 
 })();
